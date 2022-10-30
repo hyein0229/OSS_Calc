735bd6832e8b775fe03856a74cd397c344962177
hyein0229 <l1253@naver.com>
2022-10-31 02:07:29 +0900

    선택할 수 있는 연산 기능 보여주기 함수 구현

--- show_option.py
+++ show_option.py
@@ -1,4 +1,9 @@
 def print_operation():
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
 def isContinued(str):    
         
